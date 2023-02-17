711e3c60adda20ba41b720f08a8adbb7fcafc904
tom-kotlar <tomaskotlar@yahoo.co.uk>
2023-02-17 19:19:20 +0000

    Build(movie-card):MovieCardComponent

---
+++ src/app/movies/containers/movie-card/movie-card.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { Movie } from '../../models/movie.model';
+
+@Component({
+  selector: 'movie-card',
+  templateUrl: './movie-card.component.html',
+  styleUrls: ['./movie-card.component.scss']
+})
+export class MovieCardComponent {
+  @Input() movie!: Movie;
+}
