84f5f78feaa64e7dc29523fb0d3935589271018b
tom-kotlar <tomaskotlar@yahoo.co.uk>
2023-02-22 12:35:19 +0000

    Ref(series.module):Add routes forChild

--- src/app/series/series.module.ts
+++ src/app/series/series.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 import { PrimeNgModule } from '../shared/primeng.module';
 
 import { SeriesListComponent } from './series-list/series-list.component';
@@ -8,7 +7,12 @@ import { SeriesCardComponent } from './series-card/series-card.component';
 import { SeriesDetailComponent } from './series-detail/series-detail.component';
 import { SeriesFormComponent } from './series-form/series-form.component';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
+export const routes: Routes = [
+  { path: '', component: SeriesListComponent },
+  { path: 'show', component: SeriesDetailComponent },
+];
 
 @NgModule({
   declarations: [
@@ -16,12 +20,12 @@ import { FormsModule } from '@angular/forms';
     SeriesCardComponent,
     SeriesDetailComponent,
     SeriesFormComponent,
-
   ],
   imports: [
-    CommonModule, HttpClientModule, PrimeNgModule, FormsModule
-  ],
-  exports: [ SeriesListComponent, SeriesListComponent,  SeriesDetailComponent,
-    SeriesFormComponent],
+    CommonModule,
+    PrimeNgModule,
+    FormsModule,
+    RouterModule.forChild(routes),
+  ]
 })
-export class SeriesModule { }
+export class SeriesModule {}
