2d32552bc36b9987ff4971db1af07b29b6ecd671
tom-kotlar <tomaskotlar@yahoo.co.uk>
2023-02-22 12:05:55 +0000

    Re(app-routing): Children paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './pages/not-found.component';
 import { HomeComponent } from './pages/home.component';
 
+import { SeriesDetailComponent } from './series/series-detail/series-detail.component';
+import { SeriesListComponent } from './series/series-list/series-list.component';
+
 const routes: Routes = [
+
+  { path: 'series', children: [
+    { path: '', component: SeriesListComponent },
+    { path: 'show', component: SeriesDetailComponent },
+  ]},
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent },
   { path: '**', component: NotFoundComponent }
