ba6096566b5321f5c895346ec880e16826f8f27c
tom-kotlar <tomaskotlar@yahoo.co.uk>
2023-02-22 12:34:49 +0000

    Ref(movies.module):Add routesfor lazy loading

--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -1,20 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { HttpClientModule } from '@angular/common/http';
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { PrimeNgModule } from '../shared/primeng.module';
 import { MoviesListComponent } from './containers/movies-list/movies-list.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+export const routes: Routes = [
+  { path: '', component: MoviesListComponent},
+];
 
 @NgModule({
   declarations: [
    MoviesListComponent, MovieCardComponent
   ],
   imports: [
-    CommonModule, HttpClientModule, PrimeNgModule
+    CommonModule, PrimeNgModule, RouterModule.forChild(routes),
   ],
-  exports: [MoviesListComponent, MovieCardComponent],
 })
 export class MoviesModule { }
