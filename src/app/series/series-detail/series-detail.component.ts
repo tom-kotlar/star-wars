b5fc2117eaeaaa1ecf12a528906ae832a9a076a1
tom-kotlar <tomaskotlar@yahoo.co.uk>
2023-02-22 15:46:34 +0000

    Ref(series.*):Navigation card id click

--- src/app/series/series-detail/series-detail.component.ts
+++ src/app/series/series-detail/series-detail.component.ts
@@ -1,33 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { Serie } from '../serie.model';
 import { SeriesService } from '../series.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'series-detail',
   templateUrl: './series-detail.component.html',
-  styleUrls: ['./series-detail.component.scss']
+  styleUrls: ['./series-detail.component.scss'],
 })
 export class SeriesDetailComponent implements OnInit {
   serie!: any;
+  isEdit!: boolean;
 
-  constructor(private seriesService: SeriesService) {}
+  constructor(
+    private router: ActivatedRoute,
+    private seriesService: SeriesService
+  ) {}
 
   ngOnInit(): void {
+    const id = this.router.snapshot.paramMap.get('id')
+   
+    this.serie = this.seriesService
+      .readOne(id)
+      .subscribe((serie: Serie | any) => (this.serie = serie));
 
-  this.serie = this.seriesService.readOne('OB22').subscribe((serie: Serie | any) => this.serie = serie)
-}
-
-
-  onCreate(show: Serie) {
-    this.seriesService.create(show).subscribe(() => console.log('Created successfully!'))
+      this.isEdit  = this.router.snapshot.data['isEdit']
   }
 
-
+  onCreate(show: Serie) {
+    this.seriesService
+      .create(show)
+      .subscribe(() => console.log('Created successfully!'));
+  }
 
   onUpdate(serie: Serie) {
-    this.seriesService
-    .update(serie)
-    .subscribe({
+    this.seriesService.update(serie).subscribe({
       next: () => console.log('Updated successfully!'),
       error: (err) => console.log('onUpdate error:', err),
     });
@@ -35,7 +42,7 @@ export class SeriesDetailComponent implements OnInit {
 
   onDelete(serie: Serie) {
     this.seriesService
-    .delete(serie)
-    .subscribe(() => console.log('Deleted successfully!'));
+      .delete(serie)
+      .subscribe(() => console.log('Deleted successfully!'));
   }
 }
