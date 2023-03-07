fe4ceed09df8554cd40145043964a5e747dd021c
tom-kotlar <tomaskotlar@yahoo.co.uk>
2023-03-07 20:38:23 +0000

    Ref(home.ts): youtube iframe

--- src/app/core/pages/home.component.ts
+++ src/app/core/pages/home.component.ts
@@ -3,31 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'home',
   template: `
-    <div class="flex-column">
-<!--      <img-->
-<!--        src="https://media.tenor.com/L60s9x5LqsMAAAAC/star-wars-lightsaber.gif"-->
-<!--        width="100%"-->
-<!--        style="padding-top: 70px"-->
-<!--        class="flex align-items-center justify-content-center"-->
-<!--        alt="Mandalorian Starwars Intro"-->
-<!--      />-->
+      <div class="flex-column">
 
-      <iframe width="90%" height="600" class="" [src]="intro | sanitizer"
+          <iframe width="100%" height="600" class="" [src]="intro | sanitizer"
+                  title="YouTube video player"
+                  allow="accelerometer; autoplay; clipboard-write;
+                  encrypted-media; gyroscope; picture-in-picture; web-share"
+                  allowfullscreen>
+          </iframe>
 
-
-
-              allowfullscreen>
-      </iframe>
-
-      <a
-        href="https://youtu.be/8NoOsPwYacA"
-        style="text-decoration: none"
-        class="flex align-items-center justify-content-center"
-        >
-        Credits
-      </a
-      >
-    </div>
+          <a
+                  routerLink="https://youtu.be/8NoOsPwYacA"
+                  style="text-decoration: none"
+                  class="flex align-items-center justify-content-center"
+          >
+              Credits
+          </a>
+      </div>
   `,
 })
 export class HomeComponent {
